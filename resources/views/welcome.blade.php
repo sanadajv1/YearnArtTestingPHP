a944755f9c967211071372bf54f65829bcfca72e
sanadajv1 <sanadajv@student.apc.edu.ph>
2023-12-22 16:31:47 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,6 @@
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     James
-    
                 </div>
 
                 <div class="mt-16">
